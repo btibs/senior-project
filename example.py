b3adff86a7ace888962f3cc546facc227bc896a4
Beth <eam74@case.edu>
2012-04-17 14:38:52 -0400

    more testing

--- example.py
+++ example.py
@@ -2,24 +2,40 @@
 
 import sys
 #sys.path.append("/scratch/showerLib/beth/")
-sys.path.append("C:\\Users\\Beth\\My Documents\\Case\\Senior Project\\sim")
+sys.path.append("C:\\Users\\Julie\\Desktop\\beth\\Senior Project\\sim\\")
 import corsika
+import matplotlib
+
+# currently does stuff / appears to work on both DAT and CER
 
 if len(sys.argv) != 2 or sys.argv[1] == '-h':
-    print "Usage: %s <filename>"%sys.argv[0]
-    print 'For an example: %s $AUGEROFFLINEROOT/share/auger-offline/doc/SampleShowers/Corsika-1e19-6.part'%sys.argv[0]
+	print "Usage: %s <filename>"%sys.argv[0]
+	print 'For an example: %s $AUGEROFFLINEROOT/share/auger-offline/doc/SampleShowers/Corsika-1e19-6.part'%sys.argv[0]
 
-    exit(0)
+	exit(0)
 
 cors_file = corsika.CorsikaFile(sys.argv[1])
 
 cors_file.Check()
 
-for event in cors_file.GetEvents():
-    print event.GetHeader()
-    print event.GetTrailer()
-    count = 0
-    for particle in event.GetParticles():
-        count += 1
+subblocks = cors_file.GetSubBlocks()
+print subblocks
+nsb = 0
+for sb in subblocks:
+	nsb+=1
+	print corsika.RunHeader(sb)
+	print corsika.RunTrailer(sb), "events processed:",corsika.RunTrailer(sb).fEventsProcessed
+	print corsika.EventHeader(sb)
+	print corsika.EventTrailer(sb), "fParticles:", corsika.EventTrailer(sb).fParticles
+	#print "got a sb len: ", len(sb)
 
-    print count, " particles"
+print "total subblocks were %s"%nsb
+
+for event in cors_file.GetEvents():
+	print event.GetHeader()
+	print event.GetTrailer()
+	count = 0
+	for particle in event.GetParticles():
+		count += 1
+
+	print count, " particles"
